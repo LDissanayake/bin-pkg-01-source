00f7197bf4b978169cd85c9bf4caae2ce25340d6
L Dissanayake <49497142+LDissanayake@users.noreply.github.com>
2026-02-26 01:45:39 +0530

    Delete editor-readable directory

--- editor-readable/index-vendors.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'd162d13528df06972596');
--- editor-readable/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime'), 'version' => '5fd3e3eb60306a99d46e');
