aedc690461d525c48956a07a36431e5155acfc79
L Dissanayake <49497142+LDissanayake@users.noreply.github.com>
2026-02-26 01:45:54 +0530

    Delete render-readable directory

--- render-readable/render-vendors.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '72efa882154b96c101d3');
--- render-readable/render.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '3fc120223351aab8b5c0');
